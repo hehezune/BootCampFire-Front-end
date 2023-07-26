fdab8ce46bb33b4eb00f43ea3c888cb38b33cb73
kcrikana <beom0109@naver.com>
2023-07-26 12:22:43 +0900

    검색바 복구

--- bootcampfire/src/App.tsx
+++ bootcampfire/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
 import Nav from './components/Nav';
 import Board from './pages/Board';
 import BootCamp from './pages/BootCamp';
 import CampArticle from './pages/CampArticle';
 import VS from './pages/VS';
+import MainPage from './pages/MainPage/MainPage';
 
 export default function App() {
   return (
@@ -15,7 +15,7 @@ export default function App() {
       </nav>
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<MainPage />} />
           <Route path="/Board" element={<Board />} />
           <Route path="/BootCamp" element={<BootCamp />} />
           <Route path="/CampArticle" element={<CampArticle />} />
---
+++ bootcampfire/src/components/MainPage/HotBoard.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import A2Container from '../Bootcamp/Tag_Styled';
+
+export default function HotBoard() {
+  return (
+    <div>
+      <A2Container>{'자유'}</A2Container>
+      <A2Container>{'프로젝트'}</A2Container>
+      <A2Container>{'웹/연애'}</A2Container>
+      <A2Container>{'IT'}</A2Container>
+      <A2Container>{'헬스/다이어트'}</A2Container>
+      <A2Container>{'스터디'}</A2Container>
+    </div>
+  );
+}
