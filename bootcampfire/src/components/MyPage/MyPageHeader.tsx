81ef05ca913fd967ec67b0f3974810ae9387289a
bongjun <funcodezune@gmail.com>
2023-07-31 17:53:04 +0900

    Feat: MyPageHeader 구현중

--- bootcampfire/src/components/MyPage/MyPageHeader.tsx
+++ bootcampfire/src/components/MyPage/MyPageHeader.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Bold18px, Bold24px } from "components/Board/styled";
 import styled from "styled-components";
+import { Routes, Route } from 'react-router-dom';
 function MyPageHeader() {
     return (
         <>
             <Bold24px>마이페이지</Bold24px>
             <StyledDiv>
+                <Route
             <StyledBold21px as="span" onClick={handlerListClick}>개인정보</StyledBold21px>
             <StyledBold21px as="span" onClick={handlerListClick}>내가 쓴 글 보기</StyledBold21px>
             </StyledDiv>
