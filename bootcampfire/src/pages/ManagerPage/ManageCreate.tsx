70ee3bf3236c3f538a2adc3dbc3007991e6d7a1c
User <beom0109@naver.com>
2023-08-04 02:09:38 +0900

    등록 버튼 추가

---
+++ bootcampfire/src/pages/ManagerPage/ManageCreate.tsx
@@ -0,0 +1,9 @@
+const ManageCreate = () => {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+};
+
+export default ManageCreate;
--- bootcampfire/src/pages/ManagerPage/Management.tsx
+++ bootcampfire/src/pages/ManagerPage/Management.tsx
@@ -1,11 +1,21 @@
-import { Bold18px } from 'components/Board/styled';
-import ManageTable from 'components/Manager/ManageTable';
+import { Bold18px, StrongBtn } from "components/Board/styled";
+import ManageTable from "components/Manager/ManageTable";
+import { useNavigate } from "react-router-dom";
 
 export default function Managements() {
+  const navigate = useNavigate();
+  const openManageCreate = () => {
+    navigate(":bootcampid");
+  };
   return (
     <div>
-      <Bold18px style={{ marginTop: '20px', marginBottom: '20px' }}>부트캠프 등록하기</Bold18px>
+      <Bold18px style={{ marginTop: "20px", marginBottom: "20px" }}>
+        부트캠프 등록하기
+      </Bold18px>
       <ManageTable></ManageTable>
+      <StrongBtn type="first" onClick={openManageCreate}>
+        수정하기
+      </StrongBtn>
     </div>
   );
 }
