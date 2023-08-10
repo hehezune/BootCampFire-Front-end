9bd3ce740a8e82297a6e46359803654472fea5fb
sulogc <suhyeng119@khu.ac.kr>
2023-08-10 23:11:20 +0900

    fix: some proxy설정

--- bootcampfire/src/setProxy.ts
+++ bootcampfire/src/setProxy.ts
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app: Express) {
   app.use(
-    createProxyMiddleware("/games", {
+    createProxyMiddleware("/", {
       target: "http://localhost:8080",
       changeOrigin: true,
     })
