7858082b0afb2717f50e3987ecb36bac613871ba
suhyeng119 <suhyeng119@gmail.com>
2023-08-16 07:40:35 +0900

    fix: proxy와 env 다시 업로드

---
+++ bootcampfire/src/setProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    '/',
+    createProxyMiddleware({
+      target: "http://i9a408.p.ssafy.io:8080",
+      changeOrigin: true,
+    }) 
+  ); 
+}; 
