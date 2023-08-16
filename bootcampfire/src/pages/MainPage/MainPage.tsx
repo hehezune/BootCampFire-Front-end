e7f5885bb9d036fc6a15f88b9c0c83e048ac86b6
bongjun <funcodezune@gmail.com>
2023-08-16 11:02:05 +0900

    Design: MainPageCSS 재등록

--- bootcampfire/src/pages/MainPage/MainPage.tsx
+++ bootcampfire/src/pages/MainPage/MainPage.tsx
@@ -1,36 +1,36 @@
 import styled from 'styled-components';
-import CustomizedInputBase from '../../components/CustomizedInputBase'; // CustomizedInputBase 컴포넌트 import
 import HotBoard from '../../components/MainPage/HotBoard';
 import Ranking from '../../components/MainPage/Ranking';
 import SampleBoard from 'components/MainPage/SampleBoard';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 import MainSearchInput from 'components/MainPage/MainSearchInput';
-import { useSelector } from 'react-redux';
-import type { RootState } from 'store';
-import { Root } from 'react-dom/client';
-import { useNavigate } from 'react-router-dom';
+import { StyledPage } from 'pages/BoardPage/styledPage';
 
-const MainContainer = styled.div`
-  position: relative;
+const MainContainer = styled(StyledPage)`
   display: flex;
   flex-direction: row;
-  left: 80px;
 `;
 
 const BoardDiv = styled.div`
   margin-left: 100;
+  width: 100%;
+  max-width: 835px;
 `;
 
-
-
+const BoardThumbnail = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+  gap: 10%;
+`
 export default function MainPage() {
   function createData(index: number, img: string, text: string) {
     return { index, img, text };
   }
+
   const rows = [
-    createData(1, '/asd', '자유게시판'),
-    createData(2, '/asd', '썸/연애'),
+    createData(1, "/asd", '자유게시판'),
+    createData(2, "/asd", '썸/연애'),
     createData(3, '/asd', '헬스/다이어트'),
     createData(4, '/asd', '고민'),
     createData(5, '/asd', '프로젝트'),
@@ -40,23 +40,18 @@ export default function MainPage() {
     createData(9, '/asd', '내 부트캠프'),
   ];
 
-  const navigate = useNavigate();
-  const keyword = useSelector((state: RootState) => state.search.keyword);
-  const type = useSelector((state: RootState) => state.search.type);
-  
   return (
-    <MainContainer>
+    <MainContainer className='asdf'>
       <BoardDiv>
-        <h1>MainPage</h1>
         <MainSearchInput activeTitle={false}/>
         <HotBoard />
-        <div style={{ display: 'flex' }}>
+        <BoardThumbnail>
           {rows.map((row) => (
             <SampleBoard index={row.index} img={row.img} text={row.text} />
           ))}
-          <Ranking />
-        </div>
+        </BoardThumbnail>
       </BoardDiv>
+      <Ranking />
     </MainContainer>
   );
 }
