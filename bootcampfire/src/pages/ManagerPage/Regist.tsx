6c75ca30e7fc6968b0209aefa5b8fb837058d789
kcrikana <83369769+kcrikana@users.noreply.github.com>
2023-08-18 00:41:00 +0900

    Update Regist.tsx
    fix 부트캠프 무한로딩 방지

--- bootcampfire/src/pages/ManagerPage/Regist.tsx
+++ bootcampfire/src/pages/ManagerPage/Regist.tsx
@@ -1,10 +1,10 @@
-import axios from 'axios';
-import ManageCard from 'components/Manager/ManageCard';
+import axios from "axios";
+import ManageCard from "components/Manager/ManageCard";
 
-import { useEffect, useState } from 'react';
-import { setBootcampInfo } from 'store/bootcampSimpleListSlice';
-import { useDispatch } from 'react-redux';
-import styled from 'styled-components';
+import { useEffect, useState } from "react";
+import { setBootcampInfo } from "store/bootcampSimpleListSlice";
+import { useDispatch } from "react-redux";
+import styled from "styled-components";
 
 interface registBootCamp {
   imgUrl: string;
@@ -16,23 +16,28 @@ export default function Regist() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_API_URL}/users/admin/permission/list`).then((res) => {
-      console.log(res);
-      setRows(res.data.data);
-    });
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/users/admin/permission/list`)
+      .then((res) => {
+        console.log(res);
+        setRows(res.data.data);
+      });
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/bootcamps/names`)
+      .then((res) => {
+        dispatch(setBootcampInfo({ list: res.data.data }));
+      });
   }, []);
-
-  // useEffect(() => {
-  //   axios.get(`${process.env.REACT_APP_API_URL}/bootcamps/names`).then((res) => {
-  //     dispatch(setBootcampInfo({ list: res.data.data }));
-  //   });
-  // }, []);
 
   return (
     <WrapperManageCardContainer className="Wrapper">
       <ManageCardContainer className="Container">
         {rows.map((row) => (
-          <ManageCard id={row.id} img={row.imgUrl} nickname={row.nickname}></ManageCard>
+          <ManageCard
+            id={row.id}
+            img={row.imgUrl}
+            nickname={row.nickname}
+          ></ManageCard>
         ))}
       </ManageCardContainer>
     </WrapperManageCardContainer>
@@ -52,8 +57,8 @@ const ManageCardContainer = styled.div`
   align-items: center;
 `;
 function dispatch(arg0: any) {
-  throw new Error('Function not implemented.');
+  throw new Error("Function not implemented.");
 }
 function bootcampInfo(arg0: { bootcampId: any; bootcampName: any }): any {
-  throw new Error('Function not implemented.');
+  throw new Error("Function not implemented.");
 }
