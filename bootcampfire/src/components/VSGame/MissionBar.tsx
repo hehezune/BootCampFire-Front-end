90cf9d18670ae027f0f8892cd7f92753760380af
kcrikana <beom0109@naver.com>
2023-08-17 16:27:06 +0900

    fix부트캠프 불과 랭킹 오류 수정

--- bootcampfire/src/components/VSGame/MissionBar.tsx
+++ bootcampfire/src/components/VSGame/MissionBar.tsx
@@ -1,15 +1,17 @@
-import * as React from "react";
-import LinearProgress, {
-  LinearProgressProps,
-} from "@mui/material/LinearProgress";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import * as React from 'react';
+import LinearProgress, { LinearProgressProps } from '@mui/material/LinearProgress';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { useSelector } from 'react-redux';
+import { RootState } from 'store';
+import { useState } from 'react';
+import axios from 'axios';
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#FF603D",
+      main: '#FF603D',
     },
   },
 });
@@ -19,77 +21,73 @@ interface MissionBarData {
   img: string;
 }
 
-interface myBootCampRank {
-  algoCnt: string;
-}
-
-function LinearProgressWithLabel(
-  props: LinearProgressProps & { value: number }
-) {
+function LinearProgressWithLabel(props: LinearProgressProps & { value: number }) {
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{ display: "flex", alignItems: "center" }}>
-        <Box sx={{ width: "100%", mr: 1 }}>
-          <LinearProgress
-            variant="determinate"
-            color="primary"
-            {...props}
-            sx={{ height: "30px", borderRadius: 10 }}
-          />
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Box sx={{ width: '100%', mr: 1 }}>
+          <LinearProgress variant="determinate" color="primary" {...props} sx={{ height: '30px', borderRadius: 10 }} />
         </Box>
         <Box sx={{ minWidth: 35 }}>
-          <Typography variant="body2" color="text.secondary">{`${Math.round(
-            props.value
-          )}%`}</Typography>
+          <Typography variant="body2" color="text.secondary">{`${Math.round(props.value)}%`}</Typography>
         </Box>
       </Box>
     </ThemeProvider>
   );
 }
 
-export default function MissionBar(props: myBootCampRank) {
+export default function MissionBar() {
   const initialMissionData: MissionBarData = {
     num: 0,
-    img: "step0_campfire.png",
+    img: 'step0_campfire.png',
   };
-  const [progress, setProgress] =
-    React.useState<MissionBarData>(initialMissionData);
 
+  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
+  const [progress, setProgress] = React.useState<MissionBarData>(initialMissionData);
+  const accessToken = localStorage.getItem('Authorization');
   React.useEffect(() => {
-
-    setProgress(() => {
-      if (parseInt(props.algoCnt) >= 50) {
-        return {
-          num: 100,
-          img: "step4_campfire.png",
-        };
-      } else if (parseInt(props.algoCnt) >= 35) {
-        return {
-          num: parseInt(props.algoCnt) * 2,
-          img: "step3_campfire.png",
-        };
-      } else if (parseInt(props.algoCnt) >= 17) {
-        return {
-          num: parseInt(props.algoCnt) * 2,
-          img: "step2_campfire.png",
-        };
-      } else if (parseInt(props.algoCnt) > 0) {
-        return {
-          num: parseInt(props.algoCnt) * 2,
-          img: "step1_campfire.png",
-        };
-      } else {
-        return {
-          num: 0,
-          img: "step0_campfire.png",
-        };
-      }
-    });
-
+    if (isLoggedIn) {
+      axios
+        .get(`${process.env.REACT_APP_API_URL}/algorithms/algo-many/my-rank`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+          withCredentials: true,
+        })
+        .then((res) => {
+          setProgress(() => {
+            if (parseInt(res.data.data.algoCnt) >= 50) {
+              return {
+                num: 100,
+                img: 'step4_campfire.png',
+              };
+            } else if (parseInt(res.data.data.algoCnt) >= 35) {
+              return {
+                num: parseInt(res.data.data.algoCnt) * 2,
+                img: 'step3_campfire.png',
+              };
+            } else if (parseInt(res.data.data.algoCnt) >= 17) {
+              return {
+                num: parseInt(res.data.data.algoCnt) * 2,
+                img: 'step2_campfire.png',
+              };
+            } else if (parseInt(res.data.data.algoCnt) > 0) {
+              return {
+                num: parseInt(res.data.data.algoCnt) * 2,
+                img: 'step1_campfire.png',
+              };
+            } else {
+              return {
+                num: 0,
+                img: 'step0_campfire.png',
+              };
+            }
+          });
+        });
+    }
   }, []);
-
   return (
-    <Box sx={{ width: "100%" }}>
+    <Box sx={{ width: '100%' }}>
       <img src={progress.img} alt="" />
       <LinearProgressWithLabel value={progress.num} />
     </Box>
--- bootcampfire/src/pages/VSPage/MissionPage.tsx
+++ bootcampfire/src/pages/VSPage/MissionPage.tsx
@@ -1,15 +1,11 @@
-import MissionBar from "../../components/VSGame/MissionBar";
-import axios from "axios";
-import MissionData from "components/VSGame/MissionData";
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "store";
-interface bootcampList {
-  id: number;
-  name: string;
-}
+import MissionBar from '../../components/VSGame/MissionBar';
+import axios from 'axios';
+import MissionData from 'components/VSGame/MissionData';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from 'store';
+
 interface fastBootCampList {
-  algoCnt: string;
   bootcampName: string;
   rank: number;
 }
@@ -19,7 +15,6 @@ interface manyBootCampList {
   rank: number;
 }
 interface myFastBootCampList {
-  algoCnt: string;
   bootcampName: string;
   rank: number;
 }
@@ -30,50 +25,31 @@ interface myManyBootCampList {
 }
 export default function MissionPage() {
   const initialMyFastData: myFastBootCampList = {
-    algoCnt: "0",
-    bootcampName: "예비 부트캠프",
+    bootcampName: '예비 부트캠프',
     rank: 0,
   };
   const initialMyManyData: myManyBootCampList = {
-    algoCnt: "0",
-    bootcampName: "예비 부트캠프",
+    algoCnt: '0',
+    bootcampName: '예비 부트캠프',
     rank: 0,
   };
   const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
-  const bootcampId = useSelector((state: RootState) => state.auth.bootcampId);
-  const [bootcamps, setBootcamps] = useState<bootcampList[]>([]);
   const [fastBootCamps, setFastBootCamps] = useState<fastBootCampList[]>([]);
-  const [myFastBootCamps, setMyFastBootCamps] =
-    useState<myFastBootCampList>(initialMyFastData);
+  const [myFastBootCamps, setMyFastBootCamps] = useState<myFastBootCampList>(initialMyFastData);
   const [manyBootCamps, setManyBootCamps] = useState<manyBootCampList[]>([]);
-  const [myManyBootCamps, setMyManyBootCamps] =
-    useState<myManyBootCampList>(initialMyManyData);
-  const accessToken = localStorage.getItem("Authorization");
+  const [myManyBootCamps, setMyManyBootCamps] = useState<myManyBootCampList>(initialMyManyData);
+  const accessToken = localStorage.getItem('Authorization');
 
-  const [isFastInclude, setFastInclude] = useState("none");
-  const [isManyInclude, setManyInclude] = useState("none");
+  const [isFastInclude, setFastInclude] = useState('none');
+  const [isManyInclude, setManyInclude] = useState('none');
   useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_API_URL}/algorithms/algo-fifty`)
-      .then((res) => {
-        setFastBootCamps(res.data.data);
-      });
-    axios
-      .get(`${process.env.REACT_APP_API_URL}/algorithms/algo-many`)
-      .then((res) => {
-        setManyBootCamps(res.data.data);
-      });
+    axios.get(`${process.env.REACT_APP_API_URL}/algorithms/algo-fifty`).then((res) => {
+      setFastBootCamps(res.data.data);
+    });
+    axios.get(`${process.env.REACT_APP_API_URL}/algorithms/algo-many`).then((res) => {
+      setManyBootCamps(res.data.data);
+    });
     if (isLoggedIn) {
-      axios
-        .get(`${process.env.REACT_APP_API_URL}/bootcamps/lists/names`, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-          withCredentials: true,
-        })
-        .then((res) => {
-          setBootcamps(res.data.data);
-        });
       axios
         .get(`${process.env.REACT_APP_API_URL}/algorithms/algo-fifty/my-rank`, {
           headers: {
@@ -82,14 +58,9 @@ export default function MissionPage() {
           withCredentials: true,
         })
         .then((res) => {
+          // console.log(res.data);
           setMyFastBootCamps(res.data.data);
-          for (let i = 0; i < 10; i++) {
-            setFastInclude(
-              fastBootCamps[i].bootcampName === res.data.data.bootcampName
-                ? ""
-                : isFastInclude
-            );
-          }
+          if (myFastBootCamps.rank > 10) setFastInclude('');
         });
       axios
         .get(`${process.env.REACT_APP_API_URL}/algorithms/algo-many/my-rank`, {
@@ -99,69 +70,61 @@ export default function MissionPage() {
           withCredentials: true,
         })
         .then((res) => {
+          console.log(res.data);
           setMyManyBootCamps(res.data.data);
-          for (let i = 0; i < 10; i++) {
-            setManyInclude(
-              manyBootCamps[i].bootcampName === res.data.data.bootcampName
-                ? ""
-                : isManyInclude
-            );
-          }
+          if (myManyBootCamps.rank > 10) setManyInclude('');
         });
     }
   }, []);
   return (
     <div>
-      <h3 style={{ marginTop: "20px" }}>1</h3>
-      <div style={{ display: "flex" }}>
+      <h3 style={{ marginTop: '20px' }}>1</h3>
+      <div style={{ display: 'flex' }}>
         <span>
-          <MissionBar algoCnt={myFastBootCamps.algoCnt} />
+          <MissionBar />
         </span>
         <span>
-          <div style={{ display: "flex" }}>
+          <div style={{ display: 'flex' }}>
             <MissionData />
           </div>
         </span>
       </div>
-      <div style={{ display: "flex" }}>
-        <span style={{ marginLeft: "auto", marginRight: "auto" }}>
+      <div style={{ display: 'flex' }}>
+        <span style={{ marginLeft: 'auto', marginRight: 'auto' }}>
           <div>가장 불을 먼저 점화시킨 부트 캠프</div>
           <div
             style={{
-              marginTop: "20px",
-              borderBottom: "solid",
-              marginLeft: "auto",
-              color: "#94969B",
-              marginBottom: "20px",
-            }}
-          ></div>
+              marginTop: '20px',
+              borderBottom: 'solid',
+              marginLeft: 'auto',
+              color: '#94969B',
+              marginBottom: '20px',
+            }}></div>
           <table>
             <tbody>
               {fastBootCamps.map((row) => (
                 <tr key={row.rank}>
                   <td>{row.rank}</td>
                   <td>{row.bootcampName}</td>
-                  <td>{row.algoCnt}</td>
                 </tr>
               ))}
               <tr style={{ display: isFastInclude }}>
                 <td>{myFastBootCamps.rank}</td>
                 <td>{myFastBootCamps.bootcampName}</td>
-                <td>{myFastBootCamps.algoCnt}</td>
+                <td>{myManyBootCamps.algoCnt}</td>
               </tr>
             </tbody>
           </table>
         </span>
-        <span style={{ marginLeft: "auto", marginRight: "auto" }}>
+        <span style={{ marginLeft: 'auto', marginRight: 'auto' }}>
           <div>가장 많이 문제를 푼 부트 캠프</div>
           <div
             style={{
-              marginTop: "20px",
-              borderBottom: "solid",
-              marginLeft: "auto",
-              color: "#94969B",
-            }}
-          ></div>
+              marginTop: '20px',
+              borderBottom: 'solid',
+              marginLeft: 'auto',
+              color: '#94969B',
+            }}></div>
           <table>
             <tbody>
               {manyBootCamps.map((row) => (
